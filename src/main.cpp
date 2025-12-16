19867dd9211b3a602a1361c9874b1e5e0eda0cab
rantisi <rantisijaber@gmail.com>
2025-12-16 13:47:11 -0500

    Adding new tests for lexer, making sure all kinds of tokens are tokenized properly

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 
 int main() {
-    std::string test = "int main() \n { int x = 5; \n return x; }";
+    std::string test = "int main() \n { int x = 5; \n return x; return 0; }";
 
     Lexer l(test);
     l.tokenize();
